bee086368d3883ed96c3b8efecf6635ed46be205
ryhndastra <ryhndastra@gmail.com>
2026-01-13 22:07:11 +0700

    belajar tipe data string dan cara mengambil serta menghitung panjang karakter di golang

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rey")
+	fmt.Println("Reyhand")
+	fmt.Println("Reyhand Astra")
+
+	fmt.Println(len("Rey"))   //len itu buat ngitung panjang string
+	fmt.Println("Reyhand"[0]) //simbvol [] itu buat ngambil karakter pertama dari string dimulai dari 0 dalam bentuk byte contoh "String"[0] itu karakter pertama yaitu S
+	fmt.Println("Reyhand Astra"[3])
+}
